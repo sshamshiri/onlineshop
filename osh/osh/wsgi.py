0ec133f87bda46e1769c2bf37218fff1bae03d4c
sshamshiri <sarashamshiri1398@gmail.com>
2023-07-09 14:16:43 +0330

    startproject osh

--- osh/osh/wsgi.py
+++ osh/osh/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for shop project.
+WSGI config for osh project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'osh.settings')
 
 application = get_wsgi_application()
